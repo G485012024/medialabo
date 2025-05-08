9c5fa28ad2f344603b5e7f052b886cff495058bf
G485012024 <248501@st.takushoku-u.ac.jp>
2025-05-08 15:29:19 +0900

    演習3-6

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,10 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+for(let r of campus.buildingD){
+	console.log(r);
+}
+for(let s of gakka){
+	console.log(s.name);
+}
